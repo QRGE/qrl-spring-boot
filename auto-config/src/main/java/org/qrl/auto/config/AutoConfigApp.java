edc4885212c42d7047ca283d30d36eb5f9093d81
QR <1826255833@qq.com>
2022-02-15 10:54:42 +0800

    update

---
+++ auto-config/src/main/java/org/qrl/auto/config/AutoConfigApp.java
@@ -0,0 +1,16 @@
+package org.qrl.auto.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qr
+ * @date 2022/2/15 10:39
+ */
+@SpringBootApplication
+public class AutoConfigApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigApp.class, args);
+    }
+}
